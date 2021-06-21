46ead973e299dcebf82d3b52ff0ff2f0f9334f9c
Aleksey Shipilov <ashipilov@ncoi.nl>
2021-06-21 11:21:49 +0200

    Initial version

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace mssql_copy_script
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionStringSrc = new SqlConnectionStringBuilder(args[0]);
+            var connectionStringDest = new SqlConnectionStringBuilder(args[1]);
+
+            var connectionMasterDest = new SqlConnectionStringBuilder(args[1])
+            {
+                InitialCatalog = "master"
+            };
+
+            var sourceServer = new Server(new ServerConnection(new SqlConnection(connectionStringSrc.ConnectionString)));
+            using var destMasterConnection = new SqlConnection(connectionMasterDest.ConnectionString);
+            destMasterConnection.Open();
+            var destServerConnection = new ServerConnection(destMasterConnection);
+            var destServer = new Server(destServerConnection);
+
+            using var dropCmd = destMasterConnection.CreateCommand();
+            dropCmd.CommandText = $"drop database if exists [{connectionStringDest.InitialCatalog}]";
+            dropCmd.ExecuteNonQuery();
+            
+            var destDb = new Database(destServer, connectionStringDest.InitialCatalog);
+            destDb.Create();
+
+            var sourceDb = sourceServer.Databases[connectionStringSrc.InitialCatalog];
+            
+            var transfer = new Transfer(sourceDb);
+            transfer.Options.WithDependencies = true;
+            transfer.Options.ContinueScriptingOnError = true;
+
+            transfer.DestinationServerConnection = destServerConnection;
+            transfer.DestinationDatabase = connectionStringDest.InitialCatalog;
+
+            EnableCopyAllExcept(transfer,
+                nameof(transfer.CopyAllLogins),
+                nameof(transfer.CopyAllObjects),
+                nameof(transfer.CopyAllUsers));
+
+            transfer.CopySchema = true;
+            transfer.CopyData = true;
+
+            transfer.TransferData();
+        }
+
+        private static void EnableCopyAllExcept(Transfer transfer, params string[] propsToFalse)
+        {
+            var props = transfer.GetType().GetProperties().Where(p =>
+                p.PropertyType == typeof(bool) && p.Name.StartsWith("CopyAll"));
+            foreach (var prop in props)
+            {
+                var value = !propsToFalse.Contains(prop.Name);
+                prop.SetValue(transfer, value);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # copy-mssql-database
 Very simple c# code to copy a mssql database from one server to another
+
+# How to run
+
+`dotnet run -- "<source_connection_string>" "<destination_connection_string>"`
+
+for example, 
+
+`dotnet run -- "Server=src_srv;Initial Catalog=src_db;User ID=scr_usr;Password=***" "Server=.;Initial Catalog=local_db;User ID=local_usr;Password=***"`
+
+**WARNING**. The destination database will be dropped and recreated
+
